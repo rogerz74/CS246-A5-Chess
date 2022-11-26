50585441ce3f65b0be1dbdfd91e6b5dde1942e04
Roger Zhang <rogerzhangsf@gmail.com>
2022-11-26 14:56:43 -0500

    add heading for humanObserver

--- human.h
+++ human.h
@@ -1,6 +1,17 @@
 #ifndef _HUMAN_H_
 #define _HUMAN_H_
+#include "observer.h"
+#include "chessGame.h"
 
+class Human: public Observer {
+    ChessGame *chessGame;
+    std::string name;
 
+    public:
+        Human(ChessGame *chessGame, std::string name);
+        void notify();
+        ~Human();
+};
 
 #endif
+
