fc36ac9c3416b1f6bba8ec3682b4c4e428d60772
Roger Zhang <rogerzhangsf@gmail.com>
2022-11-26 14:56:31 -0500

    add heading for computerObserver

--- computer.h
+++ computer.h
@@ -1,6 +1,22 @@
 #ifndef _COMPUTER_H_
 #define _COMPUTER_H_
+#include "observer.h"
+#include "chessGame.h"
 
+class Computer: public Observer {
+    ChessGame *chessGame;
+    std::string name;
 
+    public:
+        Computer(ChessGame *chessGame, std::string name);
+        void notify();
+        // we need to decide if we want these to simply be void functions or if we want to return something
+        // This levels thing may also need to move the main.cc file??
+        void level1(ChessGame *chessGame);
+        void level2(ChessGame *chessGame);
+        void level3(ChessGame *chessGame);
+        void level4(ChessGame *chessGame);
+        ~Computer();
+};
 
 #endif
