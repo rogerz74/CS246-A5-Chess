cfb95747a89204a714b0fafc8a2ac2a37cbdddd0
s32k <105165487+s32k@users.noreply.github.com>
2022-11-29 17:33:19 -0500

    Create humanHuman.h

---
+++ humanHuman.h
@@ -0,0 +1,14 @@
+#ifndef _HUMANHUMAN_H_
+#define _HUMANHUMAN_H_
+
+#include <string>
+#include <iostream>
+#include <limits>
+#include "chessGame.h"
+#include "subject.h"
+#include "observer.h"
+#include "human.h"
+
+int humanHuman(Human *white, Human *black, ChessGame *gameBoard);
+
+#endif
