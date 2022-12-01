a464a70e1e91e54030d8c1f971c5d007eb640623
s32k <105165487+s32k@users.noreply.github.com>
2022-12-01 00:57:07 -0500

    Update gamerun.h

--- gamerun.h
+++ gamerun.h
@@ -2,11 +2,15 @@
 #define _GAMERUN_H_
 
 #include <string>
+#include <iostream>
 #include "chessGame.h"
 #include "subject.h"
 #include "observer.h"
 #include "human.h"
-#include "humanHuman.h"
+#include "computer.h"
+#include "level1.h"
+#include "level2.h"
+#include "level3.h"
 
 int gameRun(Observer *white, Observer *black, ChessGame *gameBoard);
 
