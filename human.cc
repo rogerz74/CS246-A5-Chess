023462292d994cc357256db9ad9652b653edc93b
Roger Zhang <rogerzhangsf@gmail.com>
2022-11-30 19:05:33 -0500

    feat: human notify() function changes

--- human.cc
+++ human.cc
@@ -18,9 +18,22 @@ void Human::notify() {
                 std::cout << (8-a) << " "; // printing the left-hand side row value
             } else {
                 // we will NEED TO ADD A print() function to Box Class to print either a piece, a Black tile (_) or a white tile ( )
-                std::cout << subject->getBoard()[a][b-1]->print();
+                if ((*(subject->getBoard()))[a][b-1]) {
+                    (*(subject->getBoard()))[a][b-1]->print();
+                } else { // printing black or white
+                    int val1 = a % 2;
+                    int val2 = b % 2;
+
+                    if (!val1 && !val2) {
+                        std::cout << " ";
+                    } else {
+                        std::cout << "_";
+                    }
+                }
+
             }
         }
+        std::cout << std::endl;
     }
     std::cout << std::endl;
     std::cout << "  abcdefgh" << std::endl; // printing the bottom a-h column value
