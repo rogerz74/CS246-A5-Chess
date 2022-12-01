3601f8070b496d15b492856e0ae7ff1b03bcd668
Roger Zhang <rogerzhangsf@gmail.com>
2022-11-30 19:05:59 -0500

    feat: knight redesign

--- knight.cc
+++ knight.cc
@@ -1,58 +1,67 @@
 #include "knight.h"
 
-Knight::Knight(std::string name, std::vector<std::vector<Box *>> *board, Box *box, bool whitePlayer):
-    Piece{name, board, box, whitePlayer} {}
+Knight::Knight(std::string name, std::vector<std::vector<Piece *>> *board, bool whitePlayer, int xCoord, int yCoord):
+    Piece{name, board, whitePlayer, xCoord, yCoord} {}
 
-bool Knight::isLegal(Box *targetBox) {
-    if (!(targetBox->isOccupied()) || 
-        (targetBox->isOccupied() && 
-        (checkWhitePlayer() && !(targetBox->getPiece()->checkWhitePlayer())) ||
-        (!checkWhitePlayer() && targetBox->getPiece()->checkWhitePlayer()))) {
+bool Knight::isLegal(Box &targetBox) {
 
+    // if location is EMPTY OR WHITE CAPTURE BLACK OR BLACK CAPTURE WHITE
+    if (!((*(this->getBoard()))[targetBox.getX()][targetBox.getY()]) || 
+        (((*(this->getBoard()))[targetBox.getX()][targetBox.getY()]) && 
+        ((checkWhitePlayer() && !(((*(this->getBoard()))[targetBox.getX()][targetBox.getY()])->checkWhitePlayer())) ||
+         (!checkWhitePlayer() && (((*(this->getBoard()))[targetBox.getX()][targetBox.getY()])->checkWhitePlayer())) ) )) {
         return true;
     }
     return false;
 }
 
 void Knight::updateLegalMoves() {
-    std::vector<Box*> legalMoves;
+    std::vector<Box> legalMoves;
 
-    int x = box->getX();
-    int y = box->getY();
+    int x = this->getX();
+    int y = this->getY();
 
     // if move is capturable (opposite colour) or if box is unoccupied then add to possible moves
+    Box move1(x+2, y+1);
+    Box move2(x-2, y-1);
+    Box move3(x+2, y-1);
+    Box move4(x-2, y+1);
+    Box move5(x+1, y+2);
+    Box move6(x-1, y-2);
+    Box move7(x+1, y-2);
+    Box move8(x-1, y+2);
 
-    if (isLegal((*board)[x + 2][y + 1])) {
-        legalMoves.push_back((*board)[x + 2][y + 1]);
+    if (isLegal(move1)) {
+        legalMoves.push_back(move1);
     }
 
-    if (isLegal((*board)[x - 2][y - 1])) {
-        legalMoves.push_back((*board)[x - 2][y - 1]);
+    if (isLegal(move2)) {
+        legalMoves.push_back(move2);
     }
 
-    if (isLegal((*board)[x + 2][y - 1])) {
-        legalMoves.push_back((*board)[x + 2][y - 1]);
+    if (isLegal(move3)) {
+        legalMoves.push_back(move3);
     }
 
-    if (isLegal((*board)[x - 2][y + 1])) {
-        legalMoves.push_back((*board)[x - 2][y + 1]);
-    }
-    
-    if (isLegal((*board)[x + 1][y + 2])) {
-        legalMoves.push_back((*board)[x + 1][y + 2]);
+    if (isLegal(move4)) {
+        legalMoves.push_back(move4);
     }
 
-    if (isLegal((*board)[x - 1][y - 2])) {
-        legalMoves.push_back((*board)[x - 1][y - 2]);
+    if (isLegal(move5)) {
+        legalMoves.push_back(move5);
     }
 
-    if (isLegal((*board)[x + 1][y - 2])) {
-        legalMoves.push_back((*board)[x + 1][y - 2]);
+    if (isLegal(move6)) {
+        legalMoves.push_back(move6);
     }
 
-    if (isLegal((*board)[x - 1][y + 2])) {
-        legalMoves.push_back((*board)[x - 1][y + 2]);
-    }      
+    if (isLegal(move7)) {
+        legalMoves.push_back(move7);
+    }
 
-    this->legalMoves = legalMoves;
+    if (isLegal(move8)) {
+        legalMoves.push_back(move8);
+    }
+
+    this->getLegalMoves() = &legalMoves;
 }
--- knight.h
+++ knight.h
@@ -5,11 +5,11 @@
 
 class Knight: public Piece {
     // isLegal() checks if move is capturable (opposite colour) or if box is unoccupied
-    bool isLegal(Box *targetBox);
+    bool isLegal(Box &targetBox) override;
     
     public:
-        Knight(std::string name, std::vector<std::vector<Box *>> *board, Box *box, bool whitePlayer);
-        void updateLegalMoves();
+        Knight(std::string name, std::vector<std::vector<Piece *>> *board, bool whitePlayer, int xCoord, int yCoord);
+        void updateLegalMoves() override;
 };
 
 #endif
