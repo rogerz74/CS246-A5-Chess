2fef5027e7b232e4ff282698d2f1d4148e596ceb
s32k <105165487+s32k@users.noreply.github.com>
2022-12-01 00:56:24 -0500

    Update gamerun.cc

--- gamerun.cc
+++ gamerun.cc
@@ -1,21 +1,47 @@
 #include "gamerun.h"
 using namespace std;
 
-//Helper function to redirect the board and players to apppropriate gameflow
+/*
+TO DO:
+- Add pickMove() method to Human
+- Create Levelx subclasses for Computer, each with a pickMove() method
+*/
 
 int gameRun(Observer *white, Observer *black, ChessGame *gameBoard) {
-    int winner = 0;     //1 if white wins, -1 if black wins & 0 if it's a draw
-    string W = white->name;
-    string B = black->name;
+    int turn = gameBoard->getplayerTurn();
+    int toReturn = INT_MAX;
+    string comm = "";
+    string a = "";
+    string b = "";
 
-    if ((W = "human") && (B = "human")) {
-        winner = humanHuman(white, black, gameBoard);
-    } else if (W = "human") {
-        winner = humanComp(white, black, gameBoard);    //code down, not uploaded due to ambiguity of pickMove()
-    } else if (B = "human") {
-        winner = compHuman(white, black, gameBoard);    //code down, not uploaded due to ambiguity of pickMove()
-    } else {
-        winner = compComp(white, black, gameBoard);     //code down, not uploaded due to ambiguity of pickMove()
+    while (toReturn == INT_MAX) {
+        if (turn == 1) {
+            cout << "White's Turn: ";
+            bool resignStatus = white->pickMove();       //so pickMove() will have bool return type
+            if (resignStatus == 1) {                    //never executed with compcomp
+                toReturn = -1;
+            } else if (gameBoard->isStalemate() == 1) {
+                toReturn = 0;
+            } else if (gameBoard->isBlackKingChecked() == 1) {
+                toReturn = 1;
+            } else {
+                gameBoard->switchTurn();
+                turn = gameBoard->getplayerTurn();
+            }
+        } else {
+            cout << "Black's Turn: ";
+            bool resignStatus  = black->pickMove();
+            if (resignStatus == 1) {
+                toReturn = 1;
+            } else if (gameBoard->isStalemate == 1) {
+                 toReturn = 0;
+            } else if (gameBoard->isWhiteKingChecked() == 1) {
+                toReturn = -1;
+            } else {
+                gameBoard->switchTurn();
+                turn = gameBoard->getplayerTurn();
+            }
+        }
     }
-    return winner;
+    return toReturn;
 }
