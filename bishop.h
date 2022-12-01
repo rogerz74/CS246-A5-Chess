772ba72750a7d527d0e007df48e16fe023c710a5
Roger Zhang <rogerzhangsf@gmail.com>
2022-11-30 19:03:51 -0500

    fix: bishop redesign

--- bishop.h
+++ bishop.h
@@ -5,11 +5,11 @@
 
 class Bishop: public Piece {
     // isLegal() checks if move is capturable (opposite colour)
-    bool isLegal(Box *targetBox);
+    bool isLegal(Box &targetBox) override;
 
     public:
-        Bishop(std::string name, std::vector<std::vector<Box *>> *board, Box *box, bool whitePlayer);
-        void updateLegalMoves();
+        Bishop(std::string name, std::vector<std::vector<Piece *>> *board, bool whitePlayer, int xCoord, int yCoord);
+        void updateLegalMoves() override;
 
 };
 
