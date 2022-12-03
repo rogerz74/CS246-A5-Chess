e36f8d4ddca5bd5492af62659d904f738d1e90ec
s32k <105165487+s32k@users.noreply.github.com>
2022-12-03 15:44:26 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,7 +1,10 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
 #include "chessGame.h"
 #include "subject.h"
 #include "observer.h"
-#include "human.h"
 #include "computer.h"
 #include "box.h"
 #include "piece.h"
@@ -11,42 +14,124 @@
 #include "pawn.h"
 #include "queen.h"
 #include "rook.h"
-#include <iostream>
-#include <string>
 
-using namespace std;
+#include "setup.h"
+#include "gamerun.h"
+#include "human.h"
+#include "level1.h"
+#include "level2.h"
+#include "level3.h"
 
-// a helper function that manages the initial setup and the pieces will need to created here.
-// the return type of this function will probably need to indicate which color won the game
 
-// a helper function to convert the letter to a number; I tried using enums but converting a string to an enum is difficult
-int strToCoord(char c) {
-    if (c == 'a') {
-        return 0;
-    } else if (c == 'b') {
-        return 1;
-    } else if (c == 'c') {
-        return 2;
-    } else if (c == 'd') {
-        return 3;
-    } else if (c == 'e') {
-        return 4;
-    } else if (c == 'f') {
-        return 5;
-    } else if (c == 'g') {
-        return 6;
+
+struct scoreBoard;
+void gameInstance();
+
+int main() {
+    scoreBoard tracker{0,0};
+    while (true) {
+        gameInstance();
+    }
+    tracker.print();
+}
+
+
+
+struct scoreBoard {
+    float whitePoints;
+    float blackPoints;
+    void print();
+}
+
+void scoreBoard::print() {
+    std::cout << "Final Score:" << std::endl;
+    std::cout << "White: " << scoreBoard.whitePoints << std::endl;
+    std::cout << "Black: " << scoreBoard.blackPoints << std::endl;
+}
+
+
+void buildBoard(std::vector<std::vector<Piece *>> &board) {
+    for (int row = 0; row < 8; ++row) {
+        std::vector<Piece *> boardRow;
+        for (int col = 0; col < 8; ++col) { 
+            boardRow.push_back(nullptr);        //initializing all places to nullptrs
+        } 
+        board.push_back(boardRow);
+    } 
+}
+
+
+Observer constructPlayer(ChessGame *theGame, std::string player, std::vector<Piece> *pieceSet) {
+    if (player == "human") {
+        Human humanPlayer{theGame, "human"};
+        return humanPlayer;
+    } else if (player == "computer[1]") {
+        Level1 level1Player{theGame, "level1", pieceSet};
+        return level1Player;
+    } else if (player == "computer[2]") {
+        Level2 level2Player{theGame, "level2", pieceSet};
+        return level2Player;
+    } else if (player == "computer[3]") {
+        Level3 level3Player{theGame, "level3", pieceSet};
+        return level3Player;
     } else {
-        return 7;
+        std::cout << "Invalid Player Given!" << std::endl;      //make it ask again?
+        return nullptr;
     }
 }
 
-int main() {
 
-    // this part will contain the loop that allows for multiple games of
-    // chess to be run and tracked.
+void gameInstance() {
 
+    //iterate over vector of vectors and build base board - possibility of keeping in main
+    std::vector<std::vector<Piece *>> ChessBoard;
+    buildBoard(ChessBoard);
+    std::vector<std::vector<Piece *>> emptyBoard;
+    buildBoard(emptyBoard);
 
-    
+    ChessGame game{&ChessBoard};        //constructor takes in ptr to board
+    std::vector<Piece> whitePieces;     //whitePieces & blackPieces have pieces now, instead of pointers to pieces
+    std::vector<Piece> blackPieces;
 
+    bool setupFlag = 0;         //changed to 1 if "setup" command executed (must be successful, else cannot leave mode)
+    bool gameRunFlag = 0;       //changed to 1 if "game" command executed and when gameRun finishes
+    std::string comm = "";
+    while (!(gameRunFlag) && (cin >> comm)) {
+        if (comm == "setup") {
+            if (setupFlag) {
+                ChessBoard.clear();
+                buildBoard(ChessBoard);
+                game.setBoard(&ChessBoard);    //create method to take in pointer to the board and change board in class
+            }
+            userSetup(&game, whitePieces, blackPieces);     //takes references to the vectors and changes them
+            setupFlag = 1;
+        } else if (comm == "game") {
+            std::string wPlayer = "";
+            std::string bPlayer = "";
+            std::cin >> wPlayer >> bPlayer;
 
+            if (!(setupFlag)) {
+                game.setBoard(&emptyBoard);
+                defaultSetup(&game, whitePieces, blackPieces);
+            }
+
+            Observer whitePlayer = constructPlayer(&game, wPlayer, &whitePieces);
+            Observer blackPlayer = constructPlayer(&game, bPlayer, &blackPieces);
+
+            int whoWon = gameRun(&whitePlayer, &blackPlayer, &game);
+            if (whoWon == 1) {
+                tracker.whitePoints += 1;
+            } else if (whoWon = -1) {
+                tracker.blackPoints += 1;
+            } else if (whoWon == 0) {
+                tracker.whitePoints += 0.5;
+                tracker.blackPoints += 0.5;
+            } else {
+                std::cout << "Game left unfinished!" << std::endl;      //can be accounted for later
+            }
+            gameRunFlag = 1;
+        } else {
+            std::cout << "Invalid command! Please try again!" << std::endl;
+        }
+    }
 }
