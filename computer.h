06cf95c83ce4db29005c3c9e1de29ccb7aeceebb
s32k <105165487+s32k@users.noreply.github.com>
2022-12-04 18:30:14 -0500

    Update computer.h

--- computer.h
+++ computer.h
@@ -8,9 +8,10 @@ class Computer: public Observer {
     ChessGame *subject;
     std::string name; // this is can acc probably be a str being either "Black" or "White"
     std::vector<Piece> * pieceArray; //vector array of pieces for the computer
+    std::vector<Piece> * opponentArray; //vector array of pieces of the opponent (used for level3)
 
     public:
-        Computer(ChessGame *chessGame, std::string name, std::vector <Piece*> * pieceArray);
+        Computer(ChessGame *chessGame, std::string name, std::vector<Piece> *pieceArray, std::vector<Piece> *opponentArray);
         void notify();
         virtual bool pickMove() = 0;
         ~Computer();
