3905d9b2102e24881c02c9c425c74d8823a9c1d3
s32k <105165487+s32k@users.noreply.github.com>
2022-12-05 16:46:41 -0500

    Create level2.h

---
+++ level2.h
@@ -0,0 +1,33 @@
+#ifndef _LEVEL2_H_
+#define _LEVEL2_H_
+
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <iterator>
+#include "chessGame.h"
+#include "observer.h"
+#include "computer.h"
+#include "box.h"
+#include "piece.h"
+#include "pawn.h"
+#include "rook.h"
+#include "queen.h"
+#include "king.h"
+#include "knight.h"
+#include "bishop.h"
+
+class Level2: public Computer {
+    ChessGame *subject;
+    std::string name;
+    std::vector<Piece *> *pieceArray;
+    std::vector<Piece *> *opponentArray;
+
+    public:
+        Level2(ChessGame *subject, std::string name, std::vector<Piece *> *pieceArray, std::vector<Piece *> *opponentArray);
+        bool pickMove();    
+};
+#endif
