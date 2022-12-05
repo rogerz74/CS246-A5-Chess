c3144e9bd74275f9883169e5287b5964a01564a9
y5na <y5na@uwaterloo.ca>
2022-12-04 23:23:55 -0500

    piece array to have pointers

--- level3.cc
+++ level3.cc
@@ -6,8 +6,8 @@
 #include "knight.h"
 #include "bishop.h"
 
-Level3::Level3(ChessGame *subject, std::string name, std::vector<Piece> *pieceArray, std::vector<Piece> *opponentArray):
-                Computer{subject, name, pieceArray, opponentArray} {}
+Level3::Level3(ChessGame *subject, std::string name, std::vector<Piece*> *pieceArray, std::vector<Piece*> *oppArray):
+                Computer{subject, name, pieceArray, oppArray} {}
 
 
 //create second map (filteredMap)
@@ -21,14 +21,14 @@ bool Level3::pickMove() {
     std::map<Piece *, Box> filteredMap2; // moves that are purely legal (doesn't put my king in check) and capture or checks
     std::map<Piece *, Box> regularMap; // moves that are purely legal (doesn't put my king in check) and are not special (just a move)
     std::vector<Box> toAvoid;
-    std::vector<Piece> pieces = *pieceArray;
-    std::vector<Piece> opponentPieces = *opponentArray;
+    std::vector<Piece*> pieces = *pieceArray;
+    std::vector<Piece*> opponentPieces = *opponentArray;
     int arraySize = pieces.size();
     int opponentArraySize = opponentPieces.size();
 
     // looping through all of opponent's pieces
     for (int i = 0; i < opponentArraySize; ++i) {
-        std::map<Box, int> opponentsLegalMoves = (*(opponentPieces[i]).getLegalMoves());
+        std::map<Box, int> opponentsLegalMoves = (*(*(opponentPieces[i])).getLegalMoves());
         
         // loop through the piece's legal moves
         for (auto &move: opponentsLegalMoves) { 
@@ -41,80 +41,80 @@ bool Level3::pickMove() {
 
     // checking is a move puts my king in check
     for (int i = 0; i < arraySize; ++i) {
-        std::map<Box, int> potentialMoves = (*((pieces[i]).getLegalMoves()));
+        std::map<Box, int> potentialMoves = (*((*(pieces[i])).getLegalMoves()));
         for (auto &move: potentialMoves) { 
 
             // create temporary pieces that will be moved around and then deleted
             Piece *tempPiece;
-            if ((pieces[i]).getName() == "p" || (pieces[i]).getName() == "P") {
-                tempPiece = new Pawn {(pieces[i]).getName(), subject->getBoard(), (pieces[i]).checkWhitePlayer(), (pieces[i]).getX(), (pieces[i]).getY()};
-            } else if ((pieces[i]).getName() == "r" || (pieces[i]).getName() == "R") {
-                tempPiece = new Rook {(pieces[i]).getName(), subject->getBoard(), (pieces[i]).checkWhitePlayer(), (pieces[i]).getX(), (pieces[i]).getY()};  
-            } else if ((pieces[i]).getName() == "q" || (pieces[i]).getName() == "Q") {
-                tempPiece = new Queen {(pieces[i]).getName(), subject->getBoard(), (pieces[i]).checkWhitePlayer(), (pieces[i]).getX(), (pieces[i]).getY()};
-            } else if ((pieces[i]).getName() == "k" || (pieces[i]).getName() == "K") {
-                tempPiece = new King {(pieces[i]).getName(), subject->getBoard(), (pieces[i]).checkWhitePlayer(), (pieces[i]).getX(), (pieces[i]).getY()};             
-            } else if ((pieces[i]).getName() == "n" || (pieces[i]).getName() == "N") {
-                tempPiece = new Knight {(pieces[i]).getName(), subject->getBoard(), (pieces[i]).checkWhitePlayer(), (pieces[i]).getX(), (pieces[i]).getY()};               
+            if (((*pieces[i])).getName() == "p" || (*(pieces[i])).getName() == "P") {
+                tempPiece = new Pawn {(*(pieces[i])).getName(), subject->getBoard(), (*(pieces[i])).checkWhitePlayer(), (*(pieces[i])).getX(), (*(pieces[i])).getY()};
+            } else if ((*(pieces[i])).getName() == "r" || (*(pieces[i])).getName() == "R") {
+                tempPiece = new Rook {(*(pieces[i])).getName(), subject->getBoard(), (*(pieces[i])).checkWhitePlayer(), (*(pieces[i])).getX(), (*(pieces[i])).getY()};  
+            } else if ((*(pieces[i])).getName() == "q" || (*(pieces[i])).getName() == "Q") {
+                tempPiece = new Queen {(*(pieces[i])).getName(), subject->getBoard(), (*(pieces[i])).checkWhitePlayer(), (*(pieces[i])).getX(), (*(pieces[i])).getY()};
+            } else if ((*(pieces[i])).getName() == "k" || (*(pieces[i])).getName() == "K") {
+                tempPiece = new King {(*(pieces[i])).getName(), subject->getBoard(), (*(pieces[i])).checkWhitePlayer(), (*(pieces[i])).getX(), (*(pieces[i])).getY()};             
+            } else if ((*(pieces[i])).getName() == "n" || (*(pieces[i])).getName() == "N") {
+                tempPiece = new Knight {(*(pieces[i])).getName(), subject->getBoard(), (*(pieces[i])).checkWhitePlayer(), (*(pieces[i])).getX(), (*(pieces[i])).getY()};               
             } else {
-                tempPiece = new Bishop {(pieces[i]).getName(), subject->getBoard(), (pieces[i]).checkWhitePlayer(), (pieces[i]).getX(), (pieces[i]).getY()};          
+                tempPiece = new Bishop {(*(pieces[i])).getName(), subject->getBoard(), (*(pieces[i])).checkWhitePlayer(), (*(pieces[i])).getX(), (*(pieces[i])).getY()};          
             }
 
             // if the current move is a capture we need to not loose the piece it will capture and bring it back after checking for king check
             if ((*(subject->getBoard()))[(move.first).getX()][(move.first).getY()]) {
                 Piece *temp = (*(subject->getBoard()))[(move.first).getX()][(move.first).getY()];
                 (*(subject->getBoard()))[(move.first).getX()][(move.first).getX()] = tempPiece;
-                (*(subject->getBoard()))[(pieces[i]).getX()][(pieces[i]).getY()] = nullptr;
+                (*(subject->getBoard()))[(*(pieces[i])).getX()][(*(pieces[i])).getY()] = nullptr;
                 subject->checkingForKingCheck();
 
                 // if my king is not in check after potential move is made -> add to newMap
-                if (((pieces[i]).checkWhitePlayer() && !(subject->isWhiteKingChecked())) || 
-                    (!((pieces[i]).checkWhitePlayer()) && !(subject->isBlackKingChecked()))) {
+                if (((*(pieces[i])).checkWhitePlayer() && !(subject->isWhiteKingChecked())) || 
+                    (!((*(pieces[i])).checkWhitePlayer()) && !(subject->isBlackKingChecked()))) {
 
                         if (find(toAvoid.begin(), toAvoid.end(), move.first) == toAvoid.end()) {
                             // move makes current piece captured
-                            filteredMap[&(pieces[i])] = move.first;
+                            filteredMap[&(*(pieces[i]))] = move.first;
                         }else if (move.second == 1 ||
-                            ((pieces[i]).checkWhitePlayer() && subject->isBlackKingChecked()) ||
-                            (!((pieces[i]).checkWhitePlayer()) && subject->isWhiteKingChecked())) {
+                            ((*(pieces[i])).checkWhitePlayer() && subject->isBlackKingChecked()) ||
+                            (!((*(pieces[i])).checkWhitePlayer()) && subject->isWhiteKingChecked())) {
                                 // move is a captures opposing piece or checks opposing king
                                 // need to put these in one check or else it may add the move twice
-                                filteredMap2[&(pieces[i])] = move.first;
+                                filteredMap2[&(*(pieces[i]))] = move.first;
                         } else {
-                            regularMap[&(pieces[i])] = move.first;
+                            regularMap[&(*(pieces[i]))] = move.first;
                         }
                 }
 
                 // put board back into original state
                 (*(subject->getBoard()))[(move.first).getX()][(move.first).getX()] = temp;
-                (*(subject->getBoard()))[(pieces[i]).getX()][(pieces[i]).getY()] = &(pieces[i]);
+                (*(subject->getBoard()))[(*(pieces[i])).getX()][(*(pieces[i])).getY()] = &(*(pieces[i]));
 
             } else {
                 (*(subject->getBoard()))[(move.first).getX()][(move.first).getX()] = tempPiece;
-                (*(subject->getBoard()))[(pieces[i]).getX()][(pieces[i]).getY()] = nullptr;
+                (*(subject->getBoard()))[(*(pieces[i])).getX()][(*(pieces[i])).getY()] = nullptr;
                 subject->checkingForKingCheck();
 
                 // if my king is not in check after potential move is made -> add to according filtered map
-                if (((pieces[i]).checkWhitePlayer() && !(subject->isWhiteKingChecked())) || 
-                    (!((pieces[i]).checkWhitePlayer()) && !(subject->isBlackKingChecked()))) {
+                if (((*(pieces[i])).checkWhitePlayer() && !(subject->isWhiteKingChecked())) || 
+                    (!((*(pieces[i])).checkWhitePlayer()) && !(subject->isBlackKingChecked()))) {
 
                         if (find(toAvoid.begin(), toAvoid.end(), move.first) == toAvoid.end()) {
                             // move makes current piece captured
-                            filteredMap[&(pieces[i])] = move.first;
+                            filteredMap[&(*(pieces[i]))] = move.first;
                         } else if (move.second == 1 ||
-                            ((pieces[i]).checkWhitePlayer() && subject->isBlackKingChecked()) ||
-                            (!((pieces[i]).checkWhitePlayer()) && subject->isWhiteKingChecked())) {
+                            ((*(pieces[i])).checkWhitePlayer() && subject->isBlackKingChecked()) ||
+                            (!((*(pieces[i])).checkWhitePlayer()) && subject->isWhiteKingChecked())) {
                                 // move is a captures opposing piece or checks opposing king
                                 // need to put these in one check or else it may add the move twice
-                                 filteredMap2[&(pieces[i])] = move.first;
+                                 filteredMap2[&(*(pieces[i]))] = move.first;
                         } else {
-                            regularMap[&(pieces[i])] = move.first;
+                            regularMap[&(*(pieces[i]))] = move.first;
                         }
                 }
                 
                 // put board back into original state
                 (*(subject->getBoard()))[(move.first).getX()][(move.first).getX()] = nullptr;
-                (*(subject->getBoard()))[(pieces[i]).getX()][(pieces[i]).getY()] = &(pieces[i]);
+                (*(subject->getBoard()))[(*(pieces[i])).getX()][(*(pieces[i])).getY()] = &(*(pieces[i]));
             }
 
             // put back isBlackKingChecked() and isWhiteKingChecked() to original state
