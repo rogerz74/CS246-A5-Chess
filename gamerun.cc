d116234f5b5e9e7c1aae0d5405d8b130471cd6a2
Roger Zhang <rogerzhangsf@gmail.com>
2022-12-05 20:37:30 -0500

    fix: pickMove return code fix

--- gamerun.cc
+++ gamerun.cc
@@ -24,10 +24,12 @@ int gameRun(Observer *white, Observer *black, ChessGame *gameBoard) {
                     toReturn = 0;               //no legal moves remaining
                     cout << "No move available due to Stalemate!" << endl;
                 }
-            } else {                            //status is just 1
+            } else if (status == 1) {                            //status is just 1
                 white->notify();
                 gameBoard->switchTurn();
                 turn = gameBoard->getPlayerTurn();
+            } else {
+                return -9999; //return code for incomplete game
             }
         } else {
             cout << "Black's Turn: ";
@@ -42,10 +44,12 @@ int gameRun(Observer *white, Observer *black, ChessGame *gameBoard) {
                     toReturn = 0;               //stalemate
                     cout << "No move available due to Stalemate!" << endl;
                 }
-            } else {
+            } else if (status == 1) {
                 black->notify();
                 gameBoard->switchTurn();
                 turn = gameBoard->getPlayerTurn();
+            } else {
+                return -9999; //return code for incomplete game
             }
         }
 
