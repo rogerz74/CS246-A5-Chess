ea3729d3ebf2d10ccac94b37d643489fd64f7cdf
s32k <105165487+s32k@users.noreply.github.com>
2022-12-06 18:26:11 -0500

    Update level2.cc

--- level2.cc
+++ level2.cc
@@ -137,7 +137,7 @@ int Level2::pickMove() {
     std::vector<Piece *> oppPieces = *opponentArray;
     int oppArraySize = oppPieces.size();
     for (int j = 0; j < oppArraySize; j++) {       //looping through all the pieces
-        (oppPieces[i])->setLegalMoves((oppPieces[i])->updateLegalMoves());
+        (oppPieces[j])->setLegalMoves((oppPieces[j])->updateLegalMoves());
     }
     
     // check for pawn promotion
